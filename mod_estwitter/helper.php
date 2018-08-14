9ea1aa984cf0963915fb583bd040ab9301b69473
marioarnedo98 <39102985+marioarnedo98@users.noreply.github.com>
2018-08-14 13:43:24 +0200

    Version 1.0

---
+++ mod_estwitter/helper.php
@@ -0,0 +1,20 @@
+<?php
+class ModHelloWorldHelper
+{
+    /**
+     * Retrieves the hello message
+     *
+     * @param   array  $params An object containing the module parameters
+     *
+     * @access public
+     */    
+    public static function getHello($id)
+    {
+
+    
+    }
+
+
+}
+
+?>
---
+++ mod_estwitter/tmpl/default.php
@@ -0,0 +1,5 @@
+<?php 
+// No direct access
+defined('_JEXEC') or die; ?>
+<script async="" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<a class="twitter-timeline" data-height="400" data-lang="es" data-theme="dark" href="https://twitter.com/<?php echo $id ?>">Mis Tweets</a>
